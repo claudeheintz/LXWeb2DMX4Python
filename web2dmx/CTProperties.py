51e6d911ffcc77fc96121fb2b25f9c176ce5c527
Claude Heintz <dev@macluxpro.com>
2024-07-08 10:31:08 -0400

    move options to properties file

---
+++ web2dmx/CTProperties.py
@@ -0,0 +1,49 @@
+#   CTProperties.py
+#
+#	by Claude Heintz
+#	copyright 2014 by Claude Heintz Design
+#
+#  see license included with this distribution or
+#  https://www.claudeheintzdesign.com/lx/opensource.html
+
+
+#################################################################
+#
+#	This file contains a very basic properties file parser
+#	assumes a file with key=value pairs, one to a line and no frills
+#	like quotes or escapes
+#
+#################################################################
+
+
+class CTProperties:
+
+	def __init__(self):
+		self.properties = {}
+		
+		
+	def parseString(self, string):
+		lines = string.split("\n")
+		for l in lines:
+			if ( not l.startswith("#") ):
+				pair = l.split("=")
+				if ( len(pair) == 2 ):
+					self.properties[pair[0]] = pair[1]
+			
+	def parseFile(self, filename):
+		f = open(filename, 'r')
+		contents = f.read()
+		f.close()
+		self.parseString(contents)
+		
+	def stringForKey(self, key, default=""):
+		if key in self.properties:
+			return str(self.properties[key])
+		return default
+		
+	def intForKey(self, key, default=0):
+		if key in self.properties:
+			return int(self.properties[key])
+		return default
+		
+		
--- web2dmx/web2dmx.py
+++ web2dmx/web2dmx.py
@@ -17,61 +17,107 @@
 #   
 #   example use:
 #   Use a  browser or curl to send a GET request to http://192.168.1.149:27688/?set=1x50
+#           curl -X GET http://192.168.1.149:27688/?set=1x50
 #       this sets the broadcast Art-Net sending to 192.168.1.255, address 1 to 50%
+#
 #   or, http://localhost:27688/?set=1x50_2x60
+#           curl -X GET  http://localhost:27688/?set=1x50_2x60
 #       this sets both address 1 to 50% and address 2 to 60% with a single request
 #       any number of addresses can be set with /?set=ADDRESSxPERCENT
 #       with additional _ADDRESSxPERCENT pairs separated by underscores.
-#   or, http://localhost:27688/?setl1x11_22_33_44_55
+#
+#   or, http://localhost:27688/?setl=1x11_22_33_44_55
+#           curl -X GET  http://localhost:27688/?setl=1x11_22_33_44_55
 #      set list of sequential addresses starting at address
 #         followed by x then list of values separated by underscores
 #         setl=AxV1_V2_V3..., sets (A)@V1, (A+1)@V2, (A+2)@V3, etc
 #         result of example, 1@11, 2@22, 3@33, 4@44, 5@55
+
 #
-#   Modify the Art-Net broadcast address (artip) to 10.255.255.255 for standard broadcast
-#   Modify host name with specific ip address -or- 'localhost'
-#   (localhost is only used for communicating  between apps on a single computer)
+#   Edit web2dmx properties file to make the following changes:
+#      Modify the Art-Net broadcast address (artip) to 10.255.255.255 for standard broadcast
+#      Modify host name with specific ip address -or- 'localhost'
+#         (localhost is only used for communicating  between apps on a single computer)
 #
 #################################################################
 
 from http.server import HTTPServer
-import time
 from myQueryHandler import myQueryHandler
 from ArtNet import ArtNetInterface
+from CTProperties import CTProperties
+import time
+import os
 
-#########################################
-#   hostname->local network interface address.
-#   '0.0.0.0' listens on all available addresses.
-#########################################
-hostName = "0.0.0.0"
-#########################################
-#   port->port to use for web.
-#      browser or curl url is IP_OF_COMPUTER:port/?...
-#......generally ports <1000 are reserved and shouldn't be used
-#########################################
-serverPort = 27688
-#########################################
-#   artip->broadcast address for Art-Net.
-#      depends on class of address
-#......if ip network is is 10.n.n.n, broadcast is 10.255.255.255
-#      if ip network is 192.168.1.n, broadcast is 192.168.1.255
-#########################################
-artip = "192.168.1.255"
-#########################################
-
+#######################   web2dmx class    ######################
 #################################################################
 #
 #   web2DMX handles communication between HTTP server and Art-Net.
 #
 #########################################
 class web2DMX:
+
 #########################################
 #
-#   setArtNet sets local variable for Art-Net interface object.
+#   init properties dictionary of application options
+#   from web2dmx.properties file.  Edit to change:
+#      Art-Net broadcast address
+#      hostname of network interface to use
+#      port for web server to use
 #
 #########################################
-    def setArtNet(self, iface):
-        self.artnet = iface
+    def __init__(self):
+        #get properties from file named web2dmx.properties
+        self.properties = CTProperties()
+        cpath = os.path.realpath(__file__)
+        self.appdirectory = os.path.dirname(cpath)
+        self.properties.parseFile( self.appdirectory + "/web2dmx.properties")
+        #read application options
+        self.artip = self.properties.stringForKey("artnet_broadcast_address", "10.255.255.255")
+        self.hostname = self.properties.stringForKey("hostname", "0.0.0.0")
+        self.serverport = self.properties.intForKey("server_port", 27688)
+
+#########################################
+#
+#   createArtNet makes Art-Net sender and starts it sending
+#
+#########################################
+    def createArtNet(self):
+        self.artnet_interface = ArtNetInterface(self.artip)
+        self.artnet_interface.startSending()
+
+#########################################
+#
+#   createWebServer makes web server object
+#   uses myQueryHandler class to parse received query portion of urls
+#   myQueryHandler calls back to its owner with do_set messages
+#
+#########################################
+    def createWebServer(self):
+        self.web_server = HTTPServer((self.hostname, self.serverport), myQueryHandler)
+        myQueryHandler.setOwner(self)
+
+#########################################
+#
+#   runWebServer
+#      serve_forever BLOCKS until KeyboardInterrupt throws an exception
+#
+#########################################
+    def runWebServer(self):
+        print("Starting web server http://%s:%s" % (self.hostname, self.serverport))
+        try:
+            self.web_server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+#########################################
+#
+#   closeWebServer
+#
+#########################################
+    def closeWebServer(self):
+        self.web_server.server_close()
+        print("Web server stopped.")
+
 #########################################
 #
 #   do_set sets address at value.
@@ -83,37 +129,27 @@ class web2DMX:
     def do_set(self, f, a, v):
         f.write(bytes("<p>Address %s at %s </p>" % (a, v), "utf-8"))
         d = int((float(v)/100.0) * 255.0)
-        self.artnet.setDMXValue(int(a), d)
+        self.artnet_interface.setDMXValue(int(a), d)
 
-
+####################### end web2dmx class #######################
+#################################################################
 
 #################################################################
+#######################   main program    #######################
 #
-#   main program
+#      creates web2dmx object for communicating between webserver and Art-Net.
 #      creates Art-Net interface and starts it sending DMX.
-#      creates web2dmx object for communicating between query handler and Art-Net.
 #      creates HTTPServer with handler class, 'myQueryHandler'
+#         (myQueryHandler calls back to web2dmx with do_set messages)
 #      starts webServer running waiting for a connection/request
 #
 #########################################
 
 if __name__ == "__main__":
 
-    iface = ArtNetInterface(artip)
-    iface.startSending()
-    
     web2dmx = web2DMX()
-    web2dmx.setArtNet(iface)
+    web2dmx.createArtNet()
+    web2dmx.createWebServer()
 
-
-    webServer = HTTPServer((hostName, serverPort), myQueryHandler)
-    myQueryHandler.setOwner(web2dmx)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+    web2dmx.runWebServer()
+    web2dmx.closeWebServer()
